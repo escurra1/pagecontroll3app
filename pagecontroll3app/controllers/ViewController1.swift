29d00ec714d446b858ad761b0ec78afd7a02b269
mescurra <harol.josue.gomez.ruiz@everis.com>
2018-07-03 10:32:51 -0500

    second

--- pagecontroll3app/controllers/ViewController1.swift
+++ pagecontroll3app/controllers/ViewController1.swift
@@ -14,6 +14,7 @@ class ViewController1: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("View1")
     }
 
     override func didReceiveMemoryWarning() {
